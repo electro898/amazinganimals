17c53e2de55da0e2e5482b03cf0918e88776ef42
electro898 <101351317+electro898@users.noreply.github.com>
2022-10-29 22:41:27 +0200

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -31,21 +31,6 @@ export default function Meta() {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <meta property="og:type" content="article"/>
-      <meta property="og:locale" content="en_US"/>
-      <meta property="og:description" content=" ..."/>
-      <meta property="og:site_name" content=""/>
-      <meta property="article:section" content="Animal"/>
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
-      <meta property="og:image:type" content="image/jpeg" />
-      <meta property="fb:admins" content="Admin" />
-      <meta property="article:section" content="Animal" />
-      <meta property="fb:admins" content="admin" />
-      <meta property="og:type" content="video.other" />
-      <meta property="og:locale" content="en_US" />
-      <meta property="article:publisher" content="https://www.facebook.com/animmalslove/" />
-      <meta name="keywords" content="cats, cat, kitty, kitten , puppy, dog, dogs, animals, funny cats, funny kitten, amazing cats, amazing animals, home cats, home kitten, Animated GIF" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" /> 
     </Head>
   )
