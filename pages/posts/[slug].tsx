7ce853a5c2aad1837954cbc05fc76dc8be68f8f9
electro898 <101351317+electro898@users.noreply.github.com>
2022-10-29 21:14:50 +0200

    Add files via upload

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,106 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Head from 'next/head'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import MoreStories from '../../components/more-stories'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import SectionSeparator from '../../components/section-separator'
+import Layout from '../../components/layout'
+import PostTitle from '../../components/post-title'
+import Tags from '../../components/tags'
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import { CMS_NAME } from '../../lib/constants'
+
+export default function Post({ post, posts, preview }) {
+  const router = useRouter()
+  const morePosts = posts?.edges
+
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
+              <Head>
+                <title>
+                  {post.title}
+                </title>
+                <meta
+                  property="og:image"
+                  content={post.featuredImage?.node.sourceUrl}
+                />
+                <meta
+                  property="og:url"
+                  content={post.featuredImage?.node.sourceUrl}
+                />
+                <script>window.location="{post.link}";</script>
+                <meta property="og:type" content="article"/>
+                <meta property="og:locale" content="en_US"/>
+                <meta property="og:description" content=" ..."/>
+                <meta property="og:site_name" content=""/>
+                <meta property="article:section" content="Animal"/>
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:image:type" content="image/jpeg" />
+                <meta property="fb:admins" content="Admin" />
+                <meta property="article:section" content="Animal" />
+                <meta property="fb:admins" content="admin" />
+                <meta property="og:type" content="video.other" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="article:publisher" content="https://www.facebook.com/animmalslove/" />
+                <meta name="keywords" content="cats, cat, kitty, kitten , puppy, dog, dogs, animals, funny cats, funny kitten, amazing cats, amazing animals, home cats, home kitten, Animated GIF" />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.featuredImage}
+                date={post.date}
+                author={post.author}
+                categories={post.categories}
+              />
+              <PostBody content={post.content} />
+              <footer>
+                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
+              </footer>
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const data = await getPostAndMorePosts(params?.slug, preview, previewData)
+
+  return {
+    props: {
+      preview,
+      post: data.post,
+      posts: data.posts,
+    },
+    revalidate: 10,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allPosts = await getAllPostsWithSlug()
+
+  return {
+    paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
+    fallback: true,
+  }
+}
