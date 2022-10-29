17fc294501e76cf4ba282b2a7cad8e65df48c372
electro898 <101351317+electro898@users.noreply.github.com>
2022-10-29 23:17:58 +0200

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -61,6 +61,7 @@ export default function Post({ post, posts, preview }) {
               <PostHeader
                 title={post.title}
                 coverImage={post.featuredImage}
+                date={post.date}
                 author={post.author}
                 categories={post.categories}
               />
