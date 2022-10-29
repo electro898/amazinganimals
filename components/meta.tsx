cda1dc0f9b7e220fc7316728fadaad2c8e509f45
electro898 <101351317+electro898@users.noreply.github.com>
2022-10-29 22:24:48 +0200

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -32,10 +32,6 @@ export default function Meta() {
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="og:url"
-        content="DDD"
-      />
     </Head>
   )
 }
